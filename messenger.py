975e022ef5e8347eba818def378d5bd87860a4b5
RothNRK <nkippers@noels-mbp-2.home>
2016-10-03 21:48:15 +0200

    Sending iMessages

---
+++ messenger.py
@@ -0,0 +1,20 @@
+from subprocess import Popen, PIPE
+
+
+class Message(object):
+	def __init__(self):
+		self.connection = self.make_connection()
+
+	def make_connection(self):
+		args = ['2', '2']
+		p = Popen(['osascript', '-'] + args,
+				  stdin=PIPE, stdout=PIPE, stderr=PIPE)
+		return p
+	def send_message(self, message='default message'):
+		scpt = '''
+		tell application "Messages"
+			set theBuddy to buddy "nkippers@unb.ca" of service id "4B4F0344-D715-42D0-A5A2-A3734A67CFD5"
+			send "{}" to theBuddy
+		end tell
+		'''.format(message)
+		self.connection.communicate(scpt)
