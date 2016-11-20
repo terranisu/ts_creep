9e3615af4d437b24705293a6f82f1cdd06f1ba90
RothNRK <nkippers@unb.ca>
2016-11-20 13:26:34 +0100

    Removed older files

--- ticketswap.py
+++ ticketswap.py
@@ -5,7 +5,8 @@ import numpy as np
 import messenger
 import scrapper
 from bs4 import BeautifulSoup
-import sched, time
+import sched
+import time
 
 
 class TicketSwapTrigger(object):
@@ -16,6 +17,7 @@ class TicketSwapTrigger(object):
         self.messenger = messenger.Message()
         self.scapper = scrapper.TicketSwapScrapper(self.main_url)
         self.content = None
+        self.refresh_count = 0
 
     def send_message(self, message):
         self.messenger.send_message(message)
@@ -27,10 +29,14 @@ class TicketSwapTrigger(object):
         return self.scapper.get_content()
 
     def get_pretty_content(self):
-        return BeautifulSoup(self.get_web_content())
+        return BeautifulSoup(self.get_web_content(), "lxml")
 
     def get_content_values(self):
-        self.content = self.get_pretty_content()
+        try:
+            self.content = self.get_pretty_content()
+        except:
+            self.send_message('Connection died')
+            self.run_task()
 
     def get_ticket_values(self):
         self.get_content_values()
@@ -47,27 +53,30 @@ class TicketSwapTrigger(object):
 
         return ticket_count
 
-    def refreash_content(self, s):
+    def refresh_content(self, s):
+        self.refresh_count += 1
+        print('Resfresh count: {}'.format(self.refresh_count))
         ticket_count = self.get_ticket_values()
+        print('Tickets available: {}'.format(ticket_count['offered']))
+        print('Tickets sold: {}'.format(ticket_count['sold']))
         if ticket_count['offered'] > 0:
             self.send_message('There is a ticket available')
         else:
             time_delay = randrange(self.t_min, self.t_max)
-            print('Tickets offered: {}'.format(ticket_count['offered']))
-            print('Tickets sold: {}'.format(ticket_count['sold']))
             print('Time delay: {}'.format(time_delay))
-            print('===================')
+            print('=====================')
             if ticket_count['offered'] is not np.nan:
-                s.enter(time_delay, 1, self.refreash_content, (s,))
+                s.enter(time_delay, 1, self.refresh_content, (s,))
             else:
                 self.send_message('You are a robot, increase the time')
                 print('You are a robot')
 
     def run_task(self):
         s = sched.scheduler(time.time, time.sleep)
-        s.enter(1, 1, self.refreash_content, (s,))
+        s.enter(1, 1, self.refresh_content, (s,))
         s.run()
+
 if __name__ == '__main__':
-    url = 'https://www.ticketswap.nl/event/frank-carter-the-rattlesnakes-/dae96a33-5093-4218-b2fe-3db3b2681450'
+    url = 'https://www.ticketswap.nl/event/meshuggah/14c0a986-09f6-48aa-bfd1-400e7913c967'
     concert = TicketSwapTrigger(main_url=url, t_min=60, t_max=300)
     concert.run_task()
