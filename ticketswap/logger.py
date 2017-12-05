57079c0c0ca02a92227cd3788701502883c3a579
Vadim Kadnikov <terranisu@gmail.com>
2017-12-05 10:18:14 +0100

    Delegate to logging calls

--- ticketswap/logger.py
+++ ticketswap/logger.py
@@ -1,10 +1,17 @@
+"""Defines TicketSwapLogger"""
+
 import logging
 
-class Logger(object):
+class TicketSwapLogger(object):
     def __init__(self):
         self.log = logging.getLogger(__name__)
         self.log.setLevel(logging.INFO)
-        self.log.addHandler(self.__init_handler)
+        self.log.addHandler(self.__init_handler())
+
+    def __getattr__(self, method_name):
+        return getattr(self.log, method_name)
+
+    # Private section
 
     def __init_handler(self):
       handler = logging.StreamHandler()
