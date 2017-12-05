3150a426187187e7e78b782bf4fbbce3d32be01d
Vadim Kadnikov <terranisu@gmail.com>
2017-12-05 10:19:41 +0100

    Simplify TicketSwapScrapper

--- ticketswap/scrapper.py
+++ ticketswap/scrapper.py
@@ -1,12 +1,10 @@
 import cfscrape
 
 class TicketSwapScrapper(object):
-    def __init__(self, url):
-        self.url = url
-        self.scrapper = self.get_scrapper()
+    def __init__(self):
+        self.scrapper = cfscrape.create_scraper()
 
-    def get_scrapper(self):
-        return cfscrape.create_scraper()
-
-    def get_content(self):
-        return self.scrapper.get(self.url).content
+    def get_content(self, url):
+        if url is None:
+            raise ValueError('Parameter "url" is empty')
+        return self.scrapper.get(url).content
