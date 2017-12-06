a3634cbc2019eb38a40a8f172ac26c057ab520bc
Vadim Kadnikov <terranisu@gmail.com>
2017-12-06 22:25:25 +0100

    Use Logger in Scrapper

--- ticketswap/scrapper.py
+++ ticketswap/scrapper.py
@@ -1,10 +1,15 @@
+"""Defines Scrapper"""
+
 import cfscrape
 
-class TicketSwapScrapper(object):
-    def __init__(self):
+class Scrapper(object):
+    def __init__(self, logger=None):
         self.scrapper = cfscrape.create_scraper()
+        self.logger = logger
 
     def get_content(self, url):
         if url is None:
             raise ValueError('Parameter "url" is empty')
+        if self.logger:
+            self.logger.info("Fetching content from %s", url)
         return self.scrapper.get(url).content
