009e0b6a3134f5610d9773aca054e38223f36c28
Vadim Kadnikov <terranisu@gmail.com>
2017-12-06 22:27:53 +0100

    Refactor Monitor class

--- ticketswap/monitor.py
+++ ticketswap/monitor.py
@@ -1,4 +1,4 @@
-"""Defines TicketSwapMonitor"""
+"""Defines Monitor"""
 
 import yaml
 import sched
@@ -6,26 +6,26 @@ import time
 
 from random import randrange
 from bs4 import BeautifulSoup
-from scrapper import TicketSwapScrapper
-from logger import TicketSwapLogger
+# from cart import Cart as TicketSwapCart
+from scrapper import Scrapper as TicketSwapScrapper
+from message import Message as TicketSwapMessage
 
-class TicketSwapMonitor(object):
+class Monitor(object):
     SCHEDULER_DELAY = 1
     SCHEDULER_PRIORITY = 1
 
-    def __init__(self, url, config):
+    def __init__(self, url, config, logger=None):
         self.iterations = 0
         self.url = url
-        self.limit = config.limit
-        self.verbose = config.verbose
-        self.__init_credentials(config)
-        self.logger = TicketSwapLogger()
-        self.scrapper = TicketSwapScrapper()
+        self.logger = logger
+        self.scrapper = TicketSwapScrapper(self.logger)
+        self.message = TicketSwapMessage(self.logger)
+        self.__init_from_config(config)
 
     def run(self):
-        if self.verbose:
-            self.logger.info('iteration: %s', self.iterations)
-            self.logger.info('run scheduler')
+        if self.logger:
+            self.logger.info("Iteration: %s", self.iterations)
+            self.logger.info("Scheduler in running...")
         scheduler = sched.scheduler(time.time, time.sleep)
         scheduler.enter(self.SCHEDULER_DELAY, self.SCHEDULER_PRIORITY, self.__callback, (scheduler,))
         scheduler.run()
@@ -35,34 +35,37 @@ class TicketSwapMonitor(object):
     def __callback(self, scheduler):
         self.iterations += 1
         tickets = self.__get_tickets_count()
-        if tickets['offered'] > 0:
-            if self.verbose:
-                self.logger.info('iteration: %s', self.iterations)
-                self.logger.info('Tickets available: %s', tickets['offered'])
-                self.logger.info('Tickets sold: %s', tickets['sold'])
-                self.logger.info('Tickets wanted: %s', tickets['wanted'])
-            # Add the found ticket to a cart
-            print 'Add ticket'
+        if tickets["offered"] > 0:
+            self.message.send(self.phone, "Here is a ticket")
+            if self.logger:
+                self.logger.info("Iteration: %s", self.iterations)
+                self.logger.info("Tickets available: %s", tickets["offered"])
+                self.logger.info("Tickets sold: %s", tickets["sold"])
+                self.logger.info("Tickets wanted: %s", tickets["wanted"])
+                self.logger.info("Trying to buy the ticket...")
+            # TicketSwapCart(self.url).authorize(self.user, self.password).add()
+            return
         else:
             delay = randrange(self.SCHEDULER_DELAY, self.limit)
             scheduler.enter(delay, self.SCHEDULER_PRIORITY, self.__callback, (scheduler,))
-            if self.verbose:
-                self.logger.info('iteration: %s', self.iterations)
-                self.logger.info('There are not available tickets')
-                self.logger.info('Waiting for %s sec...', delay)
+            self.message.send(self.phone, "There is no ticket")
+            if self.logger:
+                self.logger.info("Iteration: %s", self.iterations)
+                self.logger.info("There are not available tickets")
+                self.logger.info("Waiting for %s seconds...", delay)
 
     def __get_tickets_count(self):
         try:
             content = BeautifulSoup(self.scrapper.get_content(self.url), "lxml")
             elements = content.find_all("div", class_="counter-value")
             return {
-                'offered': self.__get_element_value(elements[0]),
-                'sold': self.__get_element_value(elements[1]),
-                'wanted': self.__get_element_value(elements[2])
+                "offered": self.__get_element_value(elements[0]),
+                "sold": self.__get_element_value(elements[1]),
+                "wanted": self.__get_element_value(elements[2])
             }
         except:
-            if self.verbose:
-                self.logger.info('Unable to fetch the data. Rescheduling the monitor...')
+            if self.logger:
+                self.logger.info("Unable to fetch the data. Rescheduling the monitor...")
             self.run()
 
     def __get_element_value(self, element):
@@ -71,15 +74,19 @@ class TicketSwapMonitor(object):
         except IndexError:
             return 0
 
-    def __init_credentials(self, config):
+    def __init_from_config(self, config):
+        self.limit = config.limit
         if config.file is None:
             self.user = config.user
             self.password = config.password
+            self.phone = config.phone
         else:
-            (self.user, self.password) = self.__load_config(config.file)
+            settings = self.__load_config(config.file)
+            (self.user, self.password) = settings["credentials"]
+            self.phone = settings["receivers"]["phone"]
         if self.user is None or self.password is None:
-            raise ValueError('You need to input Facebook user/password or use credentials file')
+            raise ValueError("You need to input Facebook user/password or use settings file")
 
     def __load_config(self, filename):
-        with open(filename, 'r') as ymlfile:
-            return yaml.load(ymlfile)['credentials']
+        with open(filename, "r") as ymlfile:
+            return yaml.load(ymlfile)
